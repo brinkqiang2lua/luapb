2894b2bdcfb33ca2e6efc84c9bbbbb59ac728202
brinkqiang <brink.qiang@gmail.com>
2019-12-11 13:20:10 +0800

    add

---
+++ cmake/replace.bat
@@ -0,0 +1,3 @@
+dmreplacefile --SRCFILE="ModuleImport.cmake" --TPLFILE="ModuleImport.cmake" --PATH="..\thirdparty"
+dmreplacefile --SRCFILE="ModuleCompileOptions.cmake" --TPLFILE="ModuleCompileOptions.cmake" --PATH="..\thirdparty"
+PAUSE
